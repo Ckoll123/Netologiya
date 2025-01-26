828723c0b8eba8e56c189241e9328a534e12f015
Oleg <rashpill963@gmail.com>
2025-01-26 20:59:28 +0300

    Fix mistakes in Module_3/1/2

--- Module_3/1/2/main.cpp
+++ Module_3/1/2/main.cpp
@@ -6,26 +6,30 @@ struct BankAccount{
     float moneyAmount;
 };
 
-void accountUpdate(BankAccount accountToChange, float newAmountOfMoney);
+void accountUpdate(BankAccount& accountToChange, float newAmountOfMoney);
 
 int main()
 {
     std::cout << "Задача 2. Счета" << std::endl << std::endl;
 
     BankAccount bankAccount1;
-    accountUpdate(bankAccount1, 2000);
+    std::cout << "Введите номер счёта: ";
+    std::cin >> bankAccount1.accountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> bankAccount1.nameOfOwner;
+    std::cout << "Введите баланс: ";
+    std::cin >> bankAccount1.moneyAmount;
+
+    float moneyAmountUpdate;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> moneyAmountUpdate;
+    accountUpdate(bankAccount1, moneyAmountUpdate);
+
+    std::cout << "Ваш счёт: " << bankAccount1.nameOfOwner << ", " << bankAccount1.accountNumber << ", " << bankAccount1.moneyAmount 
+              << std::endl;
 }
 
-void accountUpdate(BankAccount accountToChange, float newAmountOfMoney)
+void accountUpdate(BankAccount& accountToChange, float newAmountOfMoney)
 {
-    std::cout << "Введите номер счёта: ";
-    std::cin >> accountToChange.accountNumber;
-    std::cout << "Введите имя владельца: ";
-    std::cin >> accountToChange.nameOfOwner;
-    std::cout << "Введите баланс: ";
-    std::cin >> accountToChange.moneyAmount;
-    std::cout << "Введите новый баланс: ";
-    std::cin >> accountToChange.moneyAmount;
-    std::cout << "Ваш счёт: " << accountToChange.nameOfOwner << ", " << accountToChange.accountNumber << ", " << accountToChange.moneyAmount 
-              << std::endl;
+    accountToChange.moneyAmount = newAmountOfMoney;
 }
