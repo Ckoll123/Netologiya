12a394f7629cdc999297990f8e4daa05604ad816
Oleg <rashpill963@gmail.com>
2025-07-22 21:45:09 +0300

    Improve TCP client task

--- Module_7/4/1/TCP_Client.h
+++ Module_7/4/1/TCP_Client.h
@@ -32,7 +32,7 @@ public:
 
         _server_addr.sin_family = AF_INET;
         string ip = "172.0.0.1";
-        _server_addr.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+        _server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
         _server_addr.sin_port = htons(_port);
 
         int err_s = connect(_socket, (sockaddr*)&_server_addr, sizeof(_server_addr));
@@ -42,13 +42,6 @@ public:
             WSACleanup();
             return;
         }
-
-        // int err_s = bind(_socket, (sockaddr*)&_server_addr, sizeof(_server_addr));
-        // if(err_s == SOCKET_ERROR){
-        //     cout << "Сокет не создан" << endl;
-        //     closesocket(_socket);
-        //     WSACleanup();
-        // }
     }
 
     ~TCP_Client(){
@@ -57,21 +50,29 @@ public:
         delete[] _buffer;
     }
 
-    void send_text(){
+    bool send_text(){
         int server_size = sizeof(_server_addr);
         string hello = "Hello from TCP client";
-        send(_socket, hello.c_str(), hello.size(), 0);
-        int size_data = recv(_socket, _buffer, 32, 0);
+        auto size_data = send(_socket, hello.c_str(), hello.size(), 0);
+        if (size_data < 0){
+            return false;
+        }
+
+        size_data = recv(_socket, _buffer, sizeof(_buffer), 0);
         if(size_data > 0){
             _buffer[size_data] = '\0';
             cout << _buffer << endl;
         }
+        else{
+            return false;
+        }
+        return true;
     }
 
 private:
     WSAData _wsa_data;
     SOCKET _socket;
-    sockaddr_in _server_addr{}, _client_addr{};
+    sockaddr_in _server_addr{};
     char* _buffer = new char[32];
     int _port = 54321;
 };
